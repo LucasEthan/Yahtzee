62ebdbdc58b864671c220363b6ccf756556c25b2
Lucas Ethan <asmrit1010@gmail.com>
2021-04-03 10:33:47 +0200

    Implemented reroll method

--- dice.rb
+++ dice.rb
@@ -9,9 +9,32 @@ class Dice
   def roll(number_of_rolls = 1)
     rolls = []
     number_of_rolls.times do
-      rolls << rand(1..sides)
+      rolls << individual_roll
     end
     self.last_roll = rolls
+  end
+
+  def number_of_dice_in_last_roll
+    last_roll.count
+  end
+
+  def individual_roll
+    rand(1..sides)
+  end
+
+  def reroll(*indexes)
+    raise IndexError if indexes.any? && last_roll.empty?
+
+    indexes.each do |index|
+      raise IndexError if index >= number_of_dice_in_last_roll
+    end
+
+    roll(number_of_dice_in_last_roll) if indexes.empty?
+
+    indexes.each do |index|
+      last_roll[index] = individual_roll
+    end
+    last_roll
   end
 
   private
