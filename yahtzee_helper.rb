720cd06313c82b8eaae2b332a5018340da53702f
Lucas Ethan <asmrit1010@gmail.com>
2021-04-24 09:51:56 +0200

    Refactored helper method

--- yahtzee_helper.rb
+++ yahtzee_helper.rb
@@ -16,24 +16,16 @@ module YahtzeeHelper
   def reroll_dice(dice)
     2.times do
       rerolls = Set[]
-      show_rolls(dice)
-      print "Would you like to reroll or keep them? [Y/N]: "
+      reroll_and_show_dice(dice)
 
       break unless gets.upcase[0] == "Y"
 
       loop do
         break if rerolls.count == 5
-
-        puts "[0] Reroll all the dice." if rerolls.empty?
-        5.times do |i|
-          puts "[#{i + 1}] Reroll dice ##{i + 1} (#{dice.last_roll[i]})." unless rerolls.include? i
-        end
-        puts "[6] Continue game"
-        print "Enter your choice: "
+        menu(rerolls, dice)
         choice = gets[0].to_i - 1
         if choice == -1
-          puts "Rerolling all dice.."
-          dice.reroll
+          all_dice_rerolls(dice)
           rerolls = [0, 1, 2, 3, 4]
         elsif (0..4).include?(choice)
           if rerolls.include?(choice)
@@ -41,11 +33,35 @@ module YahtzeeHelper
             next
           end
 
-          puts "Dice ##{choice + 1} (#{dice.last_roll[choice]}) has been rerolled to #{dice.reroll(choice)[choice]}"
-          rerolls << choice
+          dice_roll(choice, dice, rerolls)
         end
         break if choice == 6
       end
     end
+  end
+
+  def menu(rerolls, dice)
+    puts "[0] Reroll all the dice." if rerolls.empty?
+    5.times do |i|
+      puts "[#{i + 1}] Reroll dice ##{i + 1} (#{dice.last_roll[i]})." unless rerolls.include? i
+    end
+    puts "[6] Continue game"
+    print "Enter your choice: "
+  end
+
+  def all_dice_rerolls(dice)
+    puts "Rerolling all dice.."
+    dice.reroll
+    puts dice
+  end
+
+  def dice_roll(choice, dice, rerolls)
+    puts "Dice ##{choice + 1} (#{dice.last_roll[choice]}) has been rerolled to #{dice.reroll(choice)[choice]}"
+    rerolls << choice
+  end
+
+  def reroll_and_show_dice(dice)
+    show_rolls(dice)
+    print "Would you like to reroll or keep them? [Y/N]: "
   end
 end
