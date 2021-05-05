54506687ca5d4d78a8c30d21d80d102287492caf
Lucas Ethan <asmrit1010@gmail.com>
2021-05-05 14:11:54 +0200

    Created categories constant hash

--- round.rb
+++ round.rb
@@ -1,6 +1,24 @@
 class Round
   attr_reader :category, :rolls
 
+  CATEGORIES = {
+    # Upper section
+    ones: "ones",
+    twos: "twos",
+    threes: "threes",
+    fours: "fours",
+    fives: "fives",
+    sixes: "sixes",
+    # Lower section
+    three_of_a_kind: "three of a kind",
+    four_of_a_kind: "four of a kind",
+    full_house: "full house",
+    small_straight: "small_straight",
+    large_straight: "large_straight",
+    yahtzee: "yahtzee",
+    chance: "chance",
+  }.freeze
+
   def initialize(category, rolls)
     @category = category
     @rolls = rolls
